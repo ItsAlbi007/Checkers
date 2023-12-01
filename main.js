cfe96446b0c52aac11e7f009892c3845e3fccdb3
albi <albijon.re@gmail.com>
2023-12-01 14:23:23 -0500

    finaly

--- main.js
+++ main.js
@@ -9,36 +9,34 @@ const board = document.querySelectorAll('.box')
 
 let lastCheckerClick 
 
+// Restlastcolorfunction resets the background color of diagonally adjacent squares for a given color either "RED" or "BLUE"
+function resetLastColor(color){
+  if(lastCheckerClick) {
+    const clickedEL = document.querySelector(`#${lastCheckerClick}`)
+    const col = parseInt(clickedEL.id[1])
+    const row = parseInt(clickedEL.id[3])
+
+let diagonalOne, diagonalTwo
+
+  if (color === "red"){
+    diagonalOne = document.querySelector(`#c${col - 1}r${row + 1}`)
+    diagonalTwo = document.querySelector(`#c${col - 1}r${row - 1}`)
+  } else if (color === "blue") {
+    diagonalOne = document.querySelector(`#c${col + 1}r${row + 1}`)
+    diagonalTwo = document.querySelector(`#c${col + 1}r${row - 1}`)
+  }
+    if (diagonalOne) {
+      diagonalOne.style.backgroundColor = ""
+    }
+    
+    if (diagonalTwo){
+      diagonalTwo.style.backgroundColor = ""
+    }
+  }
+}
 
 
-// function resetLastColor (e){
-//   console.log(e)
-//   if (lastCheckerClick){
-//     console.log(lastCheckerClick)
-//     const clickedEL = document.querySelector(`#${lastCheckerClick}`)
-//     const col = parseInt(clickedEL.id[1])
-//     const row = parseInt(clickedEL.id[3])
-//     console.log(clickedEL)
-//     if (clickedEL.child.classList.contains("red")){
-//     const diagonalOne = document.querySelector(`#c${col -1}r${row +1}`)
-//     const diagonalTwo = document.querySelector(`#c${col -1}r${row -1}`)
-//     diagonalOne.style.backgroundColor = ""
-//     diagonalTwo.style.backgroundColor = ""
-//     } else if (clickedEL.child.classList.contains("blue")){
-//       const diagonalOne = document.querySelector(`#c${col +1}r${row +1}`)
-//       const diagonalTwo = document.querySelector(`#c${col +1}r${row -1}`)
-//       diagonalOne.style.backgroundColor = ""
-//       diagonalTwo.style.backgroundColor = ""
-//     }
-//   }
-// }
-
-// how would you handle moving the colors
-// next step moving checkers!!
-
-
-const child = document.getElementById('blue-checker-9');
-
+//Event listener intialization is to set up event listner on each square and then calls funciton to inilize the event listeners.
 function initiate() {
   board.forEach((box) => {
       box.addEventListener('click', (event)=>{
@@ -49,74 +47,70 @@ function initiate() {
 }
 initiate()
 
+// the function declaration is a function names first click that takes event in parameter this function worsk when a square is clicked
 function firstClick(event){
-  console.log(event)
-  resetLastColor(event)
+  resetLastColor("red")
+  resetLastColor("blue")
+  // console.log(event)
+  // resetLastColor(event)
+  // let checker and let square declares caraivle to sore refereces to the clicked checker and its parent square
   let checker
   let square
-  // const first = event.target.id;
-  const firstChild = event.target.children;
-  const id = firstChild.id;
+  
+  // this is using checker and square too click checker and its parent square and assinging them to the checker adn square variables and it updates the lastcheckerclick variable with the id of last clicked checker
+  const firstChild = event.target.children
+  const id = firstChild.id
   if (firstChild[0]){
     checker = firstChild[0]
     square = checker.parentElement
     lastCheckerClick = checker.parentElement.id
-    // console.log(square)
-    if (checker.classList.contains("red")){
-      // console.log(checker)
-      const col = parseInt(square.id[1])
-      const row = parseInt(square.id[3])
-      const diagonalOne = document.querySelector(`#c${col -1}r${row +1}`)
-      const diagonalTwo = document.querySelector(`#c${col -1}r${row -1}`)
-      // console.log(diagonalOne)
-      // console.log(diagonalTwo)
-  } else if (checker.classList.contains("blue")){
-      // console.log(checker)
-      const col = parseInt(square.id[1])
-      const row = parseInt(square.id[3])
-      const diagonalOne = document.querySelector(`#c${col +1}r${row +1}`)
-      const diagonalTwo = document.querySelector(`#c${col +1}r${row -1}`)
-      // console.log(diagonalOne)
-      // console.log(diagonalTwo)
-    }
   } else {
-    
-    checker = event.target
-    square = checker.parentElement
+    checker = event.target;
+    square = checker.parentElement;
     lastCheckerClick = checker.parentElement.id
-    // console.log(square)
+  }
+
+
+    // console.log(lastCheckerClick)
     if (checker.classList.contains("red")){
       // console.log(checker)
+
+      // this is focusing on the red checker it retrives the coulmn and row of the clicekd square selects the diagonall squares diagonalone and two and checks if they are empty and if they are empty it highlights them yellow
       const col = parseInt(square.id[1])
       const row = parseInt(square.id[3])
       const diagonalOne = document.querySelector(`#c${col -1}r${row +1}`)
       const diagonalTwo = document.querySelector(`#c${col -1}r${row -1}`)
-      console.log(diagonalOne.innerHTML)
-      if (diagonalOne.innerHTML === ""){
+
+      if (diagonalOne && diagonalOne.innerHTML === ""){
         diagonalOne.style.backgroundColor = "yellow"
       }
-      if (diagonalTwo.innerHTML === ""){
+
+      if (diagonalTwo && diagonalTwo.innerHTML === "") {
         diagonalTwo.style.backgroundColor = "yellow"
       }
       // console.log(diagonalOne)
       // console.log(diagonalTwo)
   } else if (checker.classList.contains("blue")){
       // console.log(checker)
+
+      // and this is the blue checker it does the exact same thing as red the only diffrence is the col is +1 +1 and for red is -1 -1 
       const col = parseInt(square.id[1])
       const row = parseInt(square.id[3])
       const diagonalOne = document.querySelector(`#c${col +1}r${row +1}`)
       const diagonalTwo = document.querySelector(`#c${col +1}r${row -1}`)
-      if (diagonalOne.innerHTML === ""){
+
+      if (diagonalOne && diagonalOne.innerHTML === "") {
         diagonalOne.style.backgroundColor = "yellow"
       }
-      if (diagonalTwo.innerHTML === ""){
-        diagonalTwo.style.backgroundColor = "yellow"
-      // console.log(diagonalOne)
-      // console.log(diagonalTwo)
-      }
+
+      if (diagonalTwo && diagonalTwo.innerHTML === "") {
+      diagonalTwo.style.backgroundColor = "yellow"
+
     }
-  } 
-}
+  }
+} 
+
+// next step moving checkers!!
 
 // THE BUGG WHERE THERE IS NO  BOX ON LEFT OR RIGHT COMES UP AS AN ERROR!!!
 // we completed clicking red now we need to focus on blue and switch diagonas for blue 
